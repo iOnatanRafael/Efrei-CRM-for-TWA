2274a81d8f56c05ba1e178aa450b1c7b43f2e910
William Zhong <william.zhong@efrei.net>
2015-01-16 18:31:42 +0100

    Complétion des classes

--- TwaCRM/TwaCRM/TwaCRM.cs
+++ TwaCRM/TwaCRM/TwaCRM.cs
@@ -12,33 +12,33 @@ namespace TwaCRM{
 	public class TwaCRM {
 
 		/**
-		 * 
+		 * Constructeur
 		 */
 		public TwaCRM() {
 		}
 
 		/**
-		 * 
+		 * Contient la liste des entreprises clientes
 		 */
 		private PoolEntreprisesClientes poolEntreprisesClientes;
 
 		/**
-		 * 
+		 * Contient la liste des intérimaires
 		 */
 		private PoolInterimaires poolInterimaires;
 
 		/**
-		 * 
+		 * Contient la liste des missions
 		 */
 		private PoolMissions poolMissions;
 
 		/**
-		 * 
+		 * Contient la liste des factures
 		 */
 		private PoolFactures poolFactures;
 
 		/**
-		 * 
+		 * Contient la liste des fiches de suivie
 		 */
 		private PoolFichesDeSuivie poolFichesDeSuivie;
 
